6b3120d727a3f47015d6d4ea6f573da590b9c84b
angelo79 <33789194+angelo79@users.noreply.github.com>
2025-10-06 11:41:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -167,12 +167,12 @@ def get_colored_wind_display(max_headwind, max_tailwind, max_crosswind, max_wind
 
 st.set_page_config(layout="wide")
 
-# Crea tre colonne, useremo solo quella centrale per centrare il contenuto
-col1, col2, col3 = st.columns([1, 2, 1])
-
-with col2:
-    st.markdown("<h1 style='text-align: center;'>TOTAL STEP</h1>", unsafe_allow_html=True)
-    st.markdown("<p style='text-align: center; font-size: 0.9em;'>by: angelo.corallo@am.difesa.it</p>", unsafe_allow_html=True)
+st.markdown("""
+<div style='text-align: center;'>
+    <h1>TOTAL STEP</h1>
+    <p style='font-size: 0.9em;'>by: angelo.corallo@am.difesa.it</p>
+</div>
+""", unsafe_allow_html=True)
 
 
 st_autorefresh(interval=5 * 60 * 1000, key="auto_refresh_counter")
